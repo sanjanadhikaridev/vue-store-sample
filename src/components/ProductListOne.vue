ec01c4315b2414b6dd16cbafa4afbb6053616166
sanjan274 <adhikari.sanjan@crowdbotics.com>
2020-05-28 10:21:58 +0800

    update

---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,50 @@
+<template>
+    <div id="product-list-one">
+        <h2>Product List One</h2>
+        <ul>
+            <li v-for="product in saleProducts" v-bind:key="product">
+                <span class="name">{{product.name}}</span>
+                <span class="name">{{product.price}}</span>
+            </li>
+        </ul>
+        <button v-on:click="reducePrice(4)">Reduce Price</button>
+    </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex';
+import {mapGetters} from 'vuex';
+
+export default {
+    // props: ['products'],
+    // data() {
+    //     return {
+           
+    //     };
+    // }
+    computed: {
+        products(){
+            return this.$store.state.products;
+        },
+        // saleProducts(){
+        //     return this.$store.getters.saleProducts;
+        // }
+        ...mapGetters([
+            'saleProducts'
+        ])
+    },
+    methods: {
+        // reducePrice: function(amount){
+        //     // this.$store.commit('reducePrice');
+        //     this.$store.dispatch('reducePrice', amount);
+        // }
+        ...mapActions([
+            'reducePrice' 
+        ])
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="product-list-two">
+        <h2>Product List Two</h2>
+        <li v-for="product in saleProducts" v-bind:key="product">
+            <span class="name">{{product.name}}</span>
+            <span class="name">{{product.price}}</span>
+        </li>
+    </div>
+</template>
+
+<script>
+export default {
+    // props: ['products'],
+    // data() {
+    //     return {
+    //     };
+    // }
+    computed: {
+        products() {
+            return this.$store.state.products;
+        },
+        saleProducts(){
+            return this.$store.getters.saleProducts;
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
